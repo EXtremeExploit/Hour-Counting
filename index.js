1d2bb044a67bb64ce2078598fc51ac78311d195e
EXtremeExploit <gdextremeexploit@gmail.com>
2018-02-12 03:55:23 -0300

    fixed acces_token

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Twitter = require('twit');
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
