d935b142b4add6a27ad1ad4d1d68ec9b94860653
EXtremeExploit <gdextremeexploit@gmail.com>
2018-03-11 01:03:16 -0300

    fixing while loop

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ var client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var d = new Date();
-Tweet();
-while (true) {
-    d = new Date(); 
-    if ((d.getUTCMinutes() == '00') && (d.getUTCSeconds() == '00')) {
-        Tweet();
-    }
-}
 
 function Tweet() {
     client.post('statuses/update', {
@@ -42,3 +34,13 @@ function ResetCount() {
 }
 
 console.log('Started!');
+
+
+var d = new Date();
+Tweet();
+while (true) {
+    d = new Date();
+    if ((d.getUTCMinutes() == '00') && (d.getUTCSeconds() == '00')) {
+        Tweet();
+    }
+}
